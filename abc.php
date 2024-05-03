8ab546274561c1c35ae0df04053832ad6b74c279
Shruti5deshmukh <99110164+Shruti5deshmukh@users.noreply.github.com>
2024-05-03 09:35:50 +0530

    Update abc.php

--- abc.php
+++ abc.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_SERVER','');
+define('DB_SERVER','rdsdatabase.chg0022mo6a9.eu-north-1.rds.amazonaws.com');
 define('DB_USERNAME','admin');
 define('DB_PASSWORD','admin123');
 define('DB_DATABASE','contact');
